202aa35d08edc127abce3ae4ececc7761e2cd5a6
darryl west <darryl.west@raincitysoftware.com>
2023-09-23 10:47:46 -0700

    added 3.12 version

---
+++ vers3.12/run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# darwest@ebay.com <darryl.west>
+# 2017-07-30 09:11:16
+#
+
+cd ~/raincity/python-projects/
+
+export NAME="python3.11"
+
+# -v "$PWD":/home/dpw/
+
+docker run -it -d -u dpw --name $NAME "darrylwest/$NAME"
+
